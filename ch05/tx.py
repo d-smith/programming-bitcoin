60087a0830e6cfc71f6fd54fa4313634db4c321c
d-smith <doug.smith.mail@gmail.com>
2023-08-19 09:29:11 -0700

    Added input parsing

--- ch05/tx.py
+++ ch05/tx.py
@@ -2,8 +2,12 @@
 from helper import (
     hash256,
     int_to_little_endian,
-    little_endian_to_int
+    little_endian_to_int,
+    encode_varint,
+    read_varint
 )
+
+from script import Script
 
 
 class Tx:
@@ -34,4 +38,41 @@ class Tx:
     @classmethod
     def parse(cls, stream,testnet=False):
         version = little_endian_to_int(stream.read(4))
-        return cls(version, None, None, None, testnet=testnet)
+        num_inputs = read_varint(stream)
+        inputs = []
+        for _ in range(num_inputs):
+            inputs.append(TxIn.parse(stream))
+        return cls(version, inputs, None, None, testnet=testnet)
+    
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:  # <1>
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
+
+    @classmethod
+    def parse(cls, s):
+        '''Takes a byte stream and parses the tx_input at the start
+        return a TxIn object
+        '''
+        # prev_tx is 32 bytes, little endian
+        # prev_index is an integer in 4 bytes, little endian
+        # use Script.parse to get the ScriptSig
+        # sequence is an integer in 4 bytes, little-endian
+        # return an instance of the class (see __init__ for args)
+        prev_tx = s.read(32)[::-1]
+        prev_index = little_endian_to_int(s.read(4))
+        script_sig = Script.parse(s)
+        sequence = little_endian_to_int(s.read(4))
+        return cls(prev_tx, prev_index, script_sig, sequence)
+        
